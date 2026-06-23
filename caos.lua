86584583537742fd07c5c70cae79613d1eccd0cb
souzavz460-cmyk <souzavz460@gmail.com>
2026-06-22 21:33:53 -0300

    Update caos.lua

--- caos.lua
+++ caos.lua
@@ -1,6 +1,6 @@
 -- Snow S4zx Mod – Sistema PEGAR/TACAR veículo + todas as funções anteriores
 local KEYS_URL = "https://raw.githubusercontent.com/souzavz460-cmyk/s4zx-keys/refs/heads/main/keys.json"
-local DONO_KEY = "S4zx-DonoSupreme2026"
+local DONO_KEY = "S4zx-DonoSupreme2710"
 
 -- Tela de Login
 local function mostrarLogin()
