60918450be2eb1d41f8d10b36b2cc49705331fe6
souzavz460-cmyk <souzavz460@gmail.com>
2026-07-24 05:00:38 -0300

    Update DONO_KEY to new value

--- S4zx.lua
+++ S4zx.lua
@@ -1,7 +1,7 @@
 -- Snow S4zx Mod – Ultimate Edition (Rayfield + Novas Funções de Arma + Godmode + Silent Aim Corrigido)
 
 local KEYS_URL = "https://raw.githubusercontent.com/souzavz460-cmyk/s4zx-keys/refs/heads/main/keys.json"
-local DONO_KEY = "S4zx-DonoSupreme2026"
+local DONO_KEY = "S4zx-DonoSupreme2027"
 
 -- ========== SISTEMA DE HWID ==========
 local function getHWID()
